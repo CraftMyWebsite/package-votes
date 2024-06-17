48f15f78dd02867fe90f57ce70cf04b6625503a5
Teyir <60007549+Teyir@users.noreply.github.com>
2024-06-17 10:56:01 +0200

    [IMPROVE] Update Package settings

--- Package.php
+++ Package.php
@@ -15,7 +15,7 @@ class Package implements IPackageConfig
 
     public function version(): string
     {
-        return "0.0.4";
+        return "0.0.1";
     }
 
     public function authors(): array
