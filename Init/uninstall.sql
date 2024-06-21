9bf58f0deca6e795b844ef7f417a66c014f47fd4
Teyir <60007549+Teyir@users.noreply.github.com>
2024-06-21 11:06:07 +0200

    [IMPROVE] Add Package uninstall

---
+++ Init/uninstall.sql
@@ -0,0 +1,6 @@
+DROP TABLE IF EXISTS `cmw_votes_votepoints`;
+DROP TABLE IF EXISTS `cmw_votes_logs_rewards`;
+DROP TABLE IF EXISTS `cmw_votes_config`;
+DROP TABLE IF EXISTS `cmw_votes_votes`;
+DROP TABLE IF EXISTS `cmw_votes_sites`;
+DROP TABLE IF EXISTS `cmw_votes_rewards`;
