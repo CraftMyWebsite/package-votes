148d8466627bc8d160ec5d0630f71bcc7bd1ff39
Léo <69589034+Zomblard@users.noreply.github.com>
2024-06-30 11:20:55 +0200

    [CLEAN] vendors

--- Views/Elements/votePoints.config.inc.view.php
+++ Views/Elements/votePoints.config.inc.view.php
@@ -9,33 +9,21 @@ use CMW\Model\Shop\Payment\ShopPaymentMethodSettingsModel;
 
 <form id="votePoints" action="payments/settings" method="post">
     <?php (new SecurityManager())->insertHiddenToken(); ?>
-    <div class="row">
-        <div class="col-12 col-md-6">
-            <div class="form-group">
-                <label for="<?=$varName?>_name">Nom d'affichage :</label>
-                <input value="<?= ShopPaymentMethodSettingsModel::getInstance()->getSetting($varName.'_name') ?? "Points de votes" ?>"
-                       placeholder="Token"
-                       type="text"
-                       name="<?=$varName?>_name"
-                       id="<?=$varName?>_name"
-                       class="form-control"
-                       required>
-            </div>
-        </div>
-        <div class="col-12 col-md-6">
-            <div class="form-group">
-                <label for="<?=$varName?>_icon">Icon :</label>
-                <input value="<?= ShopPaymentMethodSettingsModel::getInstance()->getSetting($varName.'_icon') ?? "fa-solid fa-coins" ?>"
-                       placeholder="Token"
-                       type="text"
-                       name="<?=$varName?>_icon"
-                       id="<?=$varName?>_icon"
-                       class="form-control"
-                       required>
-            </div>
-        </div>
+
+<div class="grid-2">
+    <div>
+        <label for="<?=$varName?>_name">Nom d'affichage :</label>
+        <input value="<?= ShopPaymentMethodSettingsModel::getInstance()->getSetting($varName.'_name') ?? "Points de votes" ?>"
+               placeholder="Token"
+               type="text"
+               name="<?=$varName?>_name"
+               id="<?=$varName?>_name"
+               class="input"
+               required>
     </div>
-    <div class="text-center">
-        <button type="submit" class="btn btn-primary">Sauvegarder</button>
+    <div>
+        <div class="icon-picker" data-id="<?=$varName?>_icon" data-name="<?=$varName?>_icon" data-label="Icon :" data-placeholder="Sélectionner un icon" data-value="<?= ShopPaymentMethodSettingsModel::getInstance()->getSetting($varName.'_icon') ?? "fa-solid fa-coins" ?>"></div>
     </div>
+</div>
+    <button type="submit" class="btn-center btn-primary">Sauvegarder</button>
 </form>
